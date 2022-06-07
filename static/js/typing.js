4ab7ec2b83c29b8b16d24685d28cf6d8ff1ef7dc
ArtemBay <31122393+ArtemBay@users.noreply.github.com>
2022-06-07 18:42:57 +0900

    Add files via upload

---
+++ static/js/typing.js
@@ -0,0 +1,31 @@
+var type_counter = 0;
+var txt = ["HTML", "JS", "CSS", "PYTHON", "C++"];
+var speed = 250;
+var indexslovo = 0;
+
+function typeDeleter() {
+  if (type_counter >= 0) {
+    document.getElementById("typer").innerHTML = txt[indexslovo].substring(0,type_counter);
+    type_counter--;
+    setTimeout(typeDeleter, 150);
+  }
+  else {
+    type_counter = 0  
+    indexslovo++
+    if (indexslovo == txt.length)
+      indexslovo = 0
+    setTimeout(typeWriter, speed)
+  }
+}
+
+function typeWriter() {
+  if (type_counter < txt[indexslovo].length) {
+    document.getElementById("typer").innerHTML += txt[indexslovo][type_counter];
+    type_counter++;
+    setTimeout(typeWriter, speed);
+  }
+  else {
+    setTimeout(typeDeleter, 3000)
+  }
+}
+typeWriter()
